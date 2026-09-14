f393e52a1e9ece10a793c5218a55883510486f83
amirarailym <amirarailym17@gmail.com>
2026-09-14 11:27:44 -0700

    Update 01-group-by-practice.sql
    Add GROUP BY aggregate function practice

--- 01-group-by-practice.sql
+++ 01-group-by-practice.sql
@@ -8,3 +8,25 @@ SELECT
 COUNT(film_id) AS films_with_this_rental_duration
 FROM film
 GROUP BY rental_duration
+
+
+/* Practice: Analyze rental rates by replacement cost
+
+Question:
+Is the rental rate higher for films with a higher replacement cost?
+
+For each replacement cost:
+- Count the films
+- Find the average rental rate
+- Find the minimum rental rate
+- Find the maximum rental rate
+*/
+
+SELECT 
+    replacement_cost,
+    COUNT(film_id) AS count_of_films,
+    AVG(rental_rate) AS average_rental_rate,
+    MIN(rental_rate) AS cheap_rental_rate,
+    MAX(rental_rate) AS expensive_rental_rate
+FROM film
+GROUP BY replacement_cost;
